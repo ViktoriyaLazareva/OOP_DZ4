882a3d53383b4da4d037eeffcd395cf5cc24ad4a
ViktoriyaLazareva <79807224+ViktoriyaLazareva@users.noreply.github.com>
2021-07-31 10:41:11 +0300

    Add files via upload

--- ArrayInt.h
+++ ArrayInt.h
@@ -1,8 +1,6 @@
-#ifndef ARRAYINT_H
-#define ARRAYINT_H
-
+#ifndef ARRAYINT_H_INCLUDED
+#define ARRAYINT_H_INCLUDED
 #include <cassert>
-
 using namespace std;
 
 class ArrayInt
@@ -12,10 +10,10 @@ private:
     int *m_data;
 
 public:
-    ArrayInt(): m_length(0), m_data(nullptr) {}
+    ArrayInt(): m_length(0), m_data(nullptr)
+    { }
 
-    ArrayInt(int length): m_length(length)
-    {
+    ArrayInt(int length): m_length(length) {
         assert(length >= 0);
 
         if (length > 0)
@@ -24,74 +22,60 @@ public:
             m_data = nullptr;
     }
 
-    ~ArrayInt(){
+    ~ArrayInt()
+    {
         delete[] m_data;
     }
 
-    void erase(){
+    void erase() {
         delete[] m_data;
         m_data = nullptr;
         m_length = 0;
     }
-
-    int getLength(){
-        return m_length;
+    int getLength() { return m_length;
     }
-
-    int& operator[](int index){
+    int& operator[](int index) {
         assert(index >= 0 && index < m_length);
         return m_data[index];
     }
 
-    void resize(int newLength)
-    {
-        if (newLength == m_length){
+    void resize(int newLength) {
+        if (newLength == m_length)
             return;
-        }
-
-        if (newLength <= 0)
-        {
+        if (newLength <= 0) {
             erase();
             return;
         }
-
         int *data = new int[newLength];
-
-        if (m_length > 0)
-        {
-            int elementsToCopy = (newLength > m_length) ? m_length : newLength;
-
-            for (int index=0; index < elementsToCopy ; ++index)
-                data[index] = m_data[index];
+        if (m_length > 0) {
+        int elementsToCopy = (newLength > m_length) ? m_length : newLength;
+           for (int index = 0; index < elementsToCopy ; ++index) {
+            data[index] = m_data[index];
+           }
         }
-
         delete[] m_data;
         m_data = data;
         m_length = newLength;
     }
-
-        void insertBefore(int value, int index)
-    {
-        assert(index >= 0 && index <= m_length);
-
-        int *data = new int[m_length+1];
-
-        for (int before=0; before < index; ++before){
+void insertBefore(int value, int index) {
+    assert(index >= 0 && index <= m_length);
+    int *data = new int[m_length+1];
+        for (int before=0; before < index; ++before)
             data[before] = m_data[before];
-        }
-
-        data [index] = value;
-
+            data [index] = value;
         for (int after=index; after < m_length; ++after)
             data[after+1] = m_data[after];
-
         delete[] m_data;
         m_data = data;
         ++m_length;
     }
-
-    void push_back(int value){
-        insertBefore(value, m_length);
+    void push_back(int value) { insertBefore(value, m_length);
+    }
+    void Print() {
+        for(int i = 0; i < m_length; i++) {
+        cout << m_data[i] << " ";
+        }
+        cout << endl;
     }
 
     void deleteLast(int index)
@@ -99,9 +83,8 @@ public:
         assert(index >= 0 && index <= m_length);
 
         int *data = new int[m_length-1];
-
-        for (int before=0; before < index; ++before){
-            data[before] = m_data[before];
+            for (int i = 0; i < m_length-1; ++i){
+            data[i] = m_data[i];
         }
 
         delete[] m_data;
@@ -113,25 +96,17 @@ public:
         deleteLast(m_length);
     }
 
-    void deleteFirst(int index)
-    {
-        assert(index >= 0 && index <= m_length);
-
-        int *data = new int[m_length-1];
-
-        for (int before=0; before < index; ++before){
-            data[before] = m_data[before+1];
+   void pop_front(){
+      int *data = new int[m_length-1];
+        for (int i = 0; i < m_length-1; ++i){
+            data[i] = m_data[i+1];
         }
         delete[] m_data;
         m_data = data;
         --m_length;
     }
 
-    void pop_front(){
-        deleteFirst(m_length);
-    }
-
-    void sort_array(){
+    void sort_massiv(){
         int temp = m_data[0];
         for(int i = 0; i < m_length-1; i++){
                 for( int j = 0; j < m_length-1; j++){
@@ -143,15 +118,9 @@ public:
                 }
         }
     }
-
-    void print_array(){
-        assert(m_length);
-        for(int i = 0; i < m_length; ++i){
-            cout << m_data[i] << " ";
-        }
-        cout << endl;
-    }
-
 };
 
-#endif
+
+
+
+#endif // ARRAYINT_H_INCLUDED
--- main.cpp
+++ main.cpp
@@ -1,170 +1,57 @@
 #include <iostream>
-#include <windows.h>
+#include "ArrayInt.h"
 #include <vector>
 
 using namespace std;
 
-typedef enum Rank{one, two, three, four, five, six, seven, eight, nine, ten, jack, queen, king, ace};
-typedef enum suit{spade, diamond, heart, club};
-
-class Card{
-private:
-    Rank m_Rank;
-    suit m_Suit;
-    bool m_IsFaceUp;
-public:
-    Card(){}
-
-    int GetValue(){
-    return m_Rank;
+void find_diff(vector<int> &massiv) {
+int temp;
+int ii = 1;
+temp = massiv[0];
+    for(int i = 0; i < massiv.size(); i++) {
+        for (int j = 1; j < massiv.size(); j++) {
+				if (massiv[j] > temp) {
+					temp = massiv[j];
+					ii++;
+		}
+      }
     }
-
-    void Flip(){
-        if(m_IsFaceUp){
-            m_IsFaceUp = false;
-        }else{
-            m_IsFaceUp = true;
-        }
-    }
-};
-
-class Hand{
-private:
-    vector<Card*> m_Cards;
-
-public:
-    Hand(){}
-
-    void addCard(Card* pCard){
-        m_Cards.push_back(pCard);
-    }
-
-    void Clear(){
-        m_Cards.clear();
-    }
-
-    int GetTotal(){
-        int total = 0;
-        for(int i = 0 ; i < m_Cards.size(); i++){
-            total += m_Cards[i]->GetValue();
-        }
-    }
-};
-
-class GenericPlayer{
-protected:
-    string m_Name;
-public:
-    GenericPlayer(){}
-
-    virtual bool IsHitting() const = 0;
-
-    bool IsBoosted() const{
-    }
-
-    void Bust()const{
-    }
-};
-
-class Player: public GenericPlayer{
-public:
-    Player(){}
-
-    virtual bool IsHitting() const{
-    }
-
-    void Win()const{
-    }
-
-    void Lose()const{
-    }
-
-    void Push()const{
-    }
-
-};
-
-class House{
-public:
-    House(){}
-
-    virtual bool IsHitting() const{
-    }
-
-    void FlipFirstCard(){
-    }
-};
-
-class Deck{
-public:
-    Deck(){}
-
-    void Populate(){
-    }
-
-    void Shuffle(){
-    }
-
-    void Deal (Hand& aHand){
-    }
-
-    void AddItionalCards (GenericPlayer& aGenericPlayer){
-    }
-} ;
-
-class Game: public Player{
-private:
-    Deck m_Deck;
-    House m_House;
-    vector<Player> m_Player;
-public:
-    Game(vector<string> n_names){
-        for(int i = 0; i < n_names.size(); i++){
-            //GenericPlayer.m_Name = n_names[i];
-            //m_Player.size() = n_names.size();
-            //Player(name)[i] = n_names[i];
-        }
-    }
-
-    void Play(){
-    }
-
-};
+        cout << ii;
+}
 
 int main()
 {
-    cout << "\t\tWelcome to Blackjack!\n\n";
 
-    int numPlayers = 0;
-    while (numPlayers < 1 || numPlayers > 7)
-    {
-        cout << "How many players? (1 - 7): ";
-        cin >> numPlayers;
-    }
+//Task 1.4--------------------
+cout << "Task 1" << endl;
+ArrayInt new_massiv;
+new_massiv.push_back(2);
+new_massiv.push_back(1);
+new_massiv.push_back(7);
+new_massiv.push_back(4);
 
-    vector<string> names;
-    string name;
+new_massiv.Print();
 
-    for (int i = 0; i < numPlayers; ++i)
-    {
-        cout << "Enter player name: ";
-        cin >> name;
-        names.push_back(name);
-    }
-    cout << endl;
+//Task 1.3--------------------
 
-    // игровой цикл
-    Game aGame(names);
-    char again = 'y';
-    while (again != 'n' && again != 'N')
-    {
-        aGame.Play();
-        cout << "\nDo you want to play again? (Y/N): ";
-        cin >> again;
-    }
+new_massiv.sort_massiv();
+new_massiv.Print();
+
+//Task 1.1--------------------
+
+new_massiv.pop_back();
+new_massiv.Print();
+
+//Task 1.3--------------------
+new_massiv.pop_front();
+new_massiv.Print();
+
+//Task 2----------------------
+cout << "Task 2" << endl;
+vector <int> massiv = {1, 1, 2, 2, 3, 3, 4, 5};
+find_diff(massiv);
 
 
 
-    system ("Pause");
     return 0;
 }
